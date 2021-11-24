2c33d4f9d7010c463a64e1a91d4f9a9a8b2309d5
Axel LUKONGO <alukongo@e1r12p21.42.fr>
2021-11-24 17:46:36 +0100

    24/11/2021

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,30 +1,39 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include"libft.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alukongo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/07 10:06:12 by alukongo          #+#    #+#             */
+/*   Updated: 2021/07/09 12:42:53 by alukongo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int ft_atoi(char *str)
+int	ft_atoi(char *str)
 {
-  int i;
-  int mult;
-  int conv;
-  mult = 1;
-  i = 0;
-  conv = 0;
-  while((str[i] == 32) || (str[i] >= 9 && str[i] <= 13))
-    i++; 
-  while(str[i] == 45|| str[i] == 43)
-    {
-      if(str[i] == 45)
-	mult *= -1;
-      if(str[i] == 43)
-	mult *= 1;
-      i++;
-    }
-  while(str[i] >= '0' && str[i] <= '9')
-    {
-      conv = conv * 10 + str[i] - 48;
-      i++;
-    }
-  conv *= mult;
-  return(conv);
+	int	i;
+	int	signe;
+	int	nbr;
+
+	nbr = 0;
+	signe = 1;
+	i = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '+')
+			signe = signe * 1;
+		if (str[i] == '-')
+			signe = signe * (-1);
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nbr = nbr * 10 + str[i] - 48;
+		i++;
+	}
+	nbr = nbr * signe;
+	return (nbr);
 }
